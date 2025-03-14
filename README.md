ab0d611218f0419dfc872cb55fe1e15537cdbc87
XeroMz69 <169330253+XeroMz69@users.noreply.github.com>
2025-03-14 11:59:18 +0000

    lake: Import all files
    Signed-off-by: XeroMz69 <169330253+XeroMz69@users.noreply.github.com>

---
+++ README.md
@@ -0,0 +1,20 @@
+## missi_phone_global-user 14 UP1A.231005.007 V816.0.10.0.UGTMIXM release-keys
+- Manufacturer: xiaomi
+- Platform: mt6768
+- Codename: pond
+- Brand: Redmi
+- Flavor: missi_phone_global-user
+- Release Version: 14
+- Kernel Version: 5.10.209
+- Id: UP1A.231005.007
+- Incremental: V816.0.10.0.UGTMIXM
+- Tags: release-keys
+- CPU Abilist: arm64-v8a,armeabi-v7a,armeabi
+- A/B Device: true
+- Treble Device: true
+- Locale: en-GB
+- Screen Density: undefined
+- Fingerprint: Redmi/pond_global/pond:14/SP1A.210812.016/V816.0.10.0.UGTMIXM:user/release-keys
+- OTA version: 
+- Branch: missi_phone_global-user-14-UP1A.231005.007-V816.0.10.0.UGTMIXM-release-keys
+- Repo: redmi_pond_dump
---
+++ join_split_files.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+cat product/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null >> product/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk
+rm -f product/priv-app/MIUIGalleryGlobal/MIUIGalleryGlobal.apk.* 2>/dev/null
+cat product/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk.* 2>/dev/null >> product/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk
+rm -f product/priv-app/MIUIMusicGlobal/MIUIMusicGlobal.apk.* 2>/dev/null
+cat product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null >> product/priv-app/GmsCore/GmsCore.apk
+rm -f product/priv-app/GmsCore/GmsCore.apk.* 2>/dev/null
+cat product/priv-app/ExtraPhotoGlobal/ExtraPhotoGlobal.apk.* 2>/dev/null >> product/priv-app/ExtraPhotoGlobal/ExtraPhotoGlobal.apk
+rm -f product/priv-app/ExtraPhotoGlobal/ExtraPhotoGlobal.apk.* 2>/dev/null
+cat product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null >> product/priv-app/Phonesky/Phonesky.apk
+rm -f product/priv-app/Phonesky/Phonesky.apk.* 2>/dev/null
+cat product/priv-app/Velvet/Velvet.apk.* 2>/dev/null >> product/priv-app/Velvet/Velvet.apk
+rm -f product/priv-app/Velvet/Velvet.apk.* 2>/dev/null
+cat product/priv-app/MIUISecurityCenterGlobal/MIUISecurityCenterGlobal.apk.* 2>/dev/null >> product/priv-app/MIUISecurityCenterGlobal/MIUISecurityCenterGlobal.apk
+rm -f product/priv-app/MIUISecurityCenterGlobal/MIUISecurityCenterGlobal.apk.* 2>/dev/null
+cat product/priv-app/MIUIAICR/MIUIAICR.apk.* 2>/dev/null >> product/priv-app/MIUIAICR/MIUIAICR.apk
+rm -f product/priv-app/MIUIAICR/MIUIAICR.apk.* 2>/dev/null
+cat product/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null >> product/priv-app/MiuiCamera/MiuiCamera.apk
+rm -f product/priv-app/MiuiCamera/MiuiCamera.apk.* 2>/dev/null
+cat product/data-app/MIUINotes/MIUINotes.apk.* 2>/dev/null >> product/data-app/MIUINotes/MIUINotes.apk
+rm -f product/data-app/MIUINotes/MIUINotes.apk.* 2>/dev/null
+cat product/data-app/MiMediaEditor/MiMediaEditor.apk.* 2>/dev/null >> product/data-app/MiMediaEditor/MiMediaEditor.apk
+rm -f product/data-app/MiMediaEditor/MiMediaEditor.apk.* 2>/dev/null
+cat product/app/YouTube/YouTube.apk.* 2>/dev/null >> product/app/YouTube/YouTube.apk
+rm -f product/app/YouTube/YouTube.apk.* 2>/dev/null
+cat product/app/WebViewGoogle64/WebViewGoogle64.apk.* 2>/dev/null >> product/app/WebViewGoogle64/WebViewGoogle64.apk
+rm -f product/app/WebViewGoogle64/WebViewGoogle64.apk.* 2>/dev/null
+cat product/app/SpeechRecognitionAndSynthesisFromGoogle/SpeechRecognitionAndSynthesisFromGoogle.apk.* 2>/dev/null >> product/app/SpeechRecognitionAndSynthesisFromGoogle/SpeechRecognitionAndSynthesisFromGoogle.apk
+rm -f product/app/SpeechRecognitionAndSynthesisFromGoogle/SpeechRecognitionAndSynthesisFromGoogle.apk.* 2>/dev/null
+cat product/app/Photos/Photos.apk.* 2>/dev/null >> product/app/Photos/Photos.apk
+rm -f product/app/Photos/Photos.apk.* 2>/dev/null
+cat product/app/Meet/Meet.apk.* 2>/dev/null >> product/app/Meet/Meet.apk
+rm -f product/app/Meet/Meet.apk.* 2>/dev/null
+cat product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null >> product/app/LatinImeGoogle/LatinImeGoogle.apk
+rm -f product/app/LatinImeGoogle/LatinImeGoogle.apk.* 2>/dev/null
+cat product/app/TrichromeLibrary64/TrichromeLibrary64.apk.* 2>/dev/null >> product/app/TrichromeLibrary64/TrichromeLibrary64.apk
+rm -f product/app/TrichromeLibrary64/TrichromeLibrary64.apk.* 2>/dev/null
+cat product/app/Gmail2/Gmail2.apk.* 2>/dev/null >> product/app/Gmail2/Gmail2.apk
+rm -f product/app/Gmail2/Gmail2.apk.* 2>/dev/null
+cat product/app/Maps/Maps.apk.* 2>/dev/null >> product/app/Maps/Maps.apk
+rm -f product/app/Maps/Maps.apk.* 2>/dev/null
+cat vendor_boot.img.* 2>/dev/null >> vendor_boot.img
+rm -f vendor_boot.img.* 2>/dev/null
+cat system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null >> system_ext/apex/com.android.vndk.v30.apex
+rm -f system_ext/apex/com.android.vndk.v30.apex.* 2>/dev/null
+cat system_ext/app/MiraVision/MiraVision.apk.* 2>/dev/null >> system_ext/app/MiraVision/MiraVision.apk
+rm -f system_ext/app/MiraVision/MiraVision.apk.* 2>/dev/null
+cat system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null >> system_ext/priv-app/Settings/Settings.apk
+rm -f system_ext/priv-app/Settings/Settings.apk.* 2>/dev/null
+cat mi_ext/product/priv-app/MIBrowserGlobal_builtin_before_2021/MIBrowserGlobal_builtin_before_2021.apk.* 2>/dev/null >> mi_ext/product/priv-app/MIBrowserGlobal_builtin_before_2021/MIBrowserGlobal_builtin_before_2021.apk
+rm -f mi_ext/product/priv-app/MIBrowserGlobal_builtin_before_2021/MIBrowserGlobal_builtin_before_2021.apk.* 2>/dev/null
+cat mi_ext/product/priv-app/GoogleDialer_arm64/GoogleDialer_arm64.apk.* 2>/dev/null >> mi_ext/product/priv-app/GoogleDialer_arm64/GoogleDialer_arm64.apk
+rm -f mi_ext/product/priv-app/GoogleDialer_arm64/GoogleDialer_arm64.apk.* 2>/dev/null
+cat mi_ext/product/priv-app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null >> mi_ext/product/priv-app/MIUIVideoPlayer/MIUIVideoPlayer.apk
+rm -f mi_ext/product/priv-app/MIUIVideoPlayer/MIUIVideoPlayer.apk.* 2>/dev/null
+cat mi_ext/product/priv-app/Messages_arm64_hdpi/Messages_arm64_hdpi.apk.* 2>/dev/null >> mi_ext/product/priv-app/Messages_arm64_hdpi/Messages_arm64_hdpi.apk
+rm -f mi_ext/product/priv-app/Messages_arm64_hdpi/Messages_arm64_hdpi.apk.* 2>/dev/null
+cat mi_ext/product/data-app/MISTORE_OVERSEA/MISTORE_OVERSEA.apk.* 2>/dev/null >> mi_ext/product/data-app/MISTORE_OVERSEA/MISTORE_OVERSEA.apk
+rm -f mi_ext/product/data-app/MISTORE_OVERSEA/MISTORE_OVERSEA.apk.* 2>/dev/null
+cat mi_ext/product/data-app/SmartHome/SmartHome.apk.* 2>/dev/null >> mi_ext/product/data-app/SmartHome/SmartHome.apk
+rm -f mi_ext/product/data-app/SmartHome/SmartHome.apk.* 2>/dev/null
+cat system/system/apex/com.android.virt.apex.* 2>/dev/null >> system/system/apex/com.android.virt.apex
+rm -f system/system/apex/com.android.virt.apex.* 2>/dev/null
+cat boot.img.* 2>/dev/null >> boot.img
+rm -f boot.img.* 2>/dev/null
